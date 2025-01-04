cffe8c028f5533e0cac73ae98704cdc0f5b55d6e
Riaan <riaan.ve2@gmail.com>
2025-01-05 01:21:10 +0200

    successfull merge

--- app/src/main/java/com/example/topics2/ui/components/addTopic/addTopicName.kt
+++ app/src/main/java/com/example/topics2/ui/components/addTopic/addTopicName.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
