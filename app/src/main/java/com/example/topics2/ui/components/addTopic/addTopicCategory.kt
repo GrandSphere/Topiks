919970a5972056cbaf51b36aaa58f33b86d96a66
Riaan <riaan.ve2@gmail.com>
2025-01-04 21:46:37 +0200

    Update

--- app/src/main/java/com/example/topics2/ui/components/addTopic/addTopicCategory.kt
+++ app/src/main/java/com/example/topics2/ui/components/addTopic/addTopicCategory.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.topics2.ui.viewmodels.topicViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
