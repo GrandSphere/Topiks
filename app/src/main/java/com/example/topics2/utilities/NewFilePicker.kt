d27faa9a93391291b739035d3fe4ef6fddc2f9a3
Riaan <riaan.ve2@gmail.com>
2025-01-07 18:59:03 +0200

    Update

--- app/src/main/java/com/example/topics2/utilities/NewFilePicker.kt
+++ app/src/main/java/com/example/topics2/utilities/NewFilePicker.kt
@@ -1,26 +1,10 @@
 import android.content.Intent
 import android.net.Uri
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
 
 // FilePicker composable
 // Utility function for handling the file picking
@@ -50,3 +34,30 @@ fun myFilePicker(
     // Return the launcher to be used outside
     return openFileLauncher
 }
+
+@Composable
+fun multipleFilePicker(
+    onFilesSelected: (List<Uri>?) -> Unit,
+    fileTypes: Array<String> = arrayOf("*/*") // Default file types
+): ActivityResultLauncher<Array<String>> {
+    val context = LocalContext.current
+    val openFilesLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.OpenMultipleDocuments(),
+        onResult = { uris: List<Uri>? ->
+            if (uris.isNullOrEmpty()) {
+                // If no files are selected, return null
+                onFilesSelected(null)
+            } else {
+                // Persist access permissions for each URI
+                val takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                uris.forEach { uri ->
+                    context.contentResolver.takePersistableUriPermission(uri, takeFlags)
+                }
+                // Return the selected file URIs to the callback
+                onFilesSelected(uris)
+            }
+        }
+    )
+    return openFilesLauncher
+}
+
