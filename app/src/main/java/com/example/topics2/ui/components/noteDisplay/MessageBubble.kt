6c0f5ad4991ae2330beda526f2d386b9dae3a8e2
ichheiseai <ichheeiseai@proton.me>
2025-01-08 15:04:50 +0200

    Update

--- app/src/main/java/com/example/topics2/ui/components/noteDisplay/MessageBubble.kt
+++ app/src/main/java/com/example/topics2/ui/components/noteDisplay/MessageBubble.kt
@@ -1,5 +1,6 @@
 package com.example.topics2.ui.components.noteDisplay
 
+import androidx.compose.ui.Modifier
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -21,7 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+//import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalClipboardManager
@@ -74,7 +77,9 @@ fun MessageBubble(
             shape = RoundedCornerShape(8.dp),
             //color = topicColor,
             color = topicColor,
-            modifier = Modifier.padding(1.dp),
+            modifier = Modifier.padding(1.dp)
+                //.widthIn(max = 100.dp)
+            ,
             tonalElevation = 0.dp, // Remove shadow
             border = null // Remove border
         ) {
