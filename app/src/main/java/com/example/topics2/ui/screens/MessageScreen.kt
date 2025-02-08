9ad77883c39acbfc59a895759d782883aa988fbe
Riaan <riaan.ve2@gmail.com>
2025-02-08 18:00:06 +0200

    working on issue

--- app/src/main/java/com/example/topics2/ui/screens/MessageScreen.kt
+++ app/src/main/java/com/example/topics2/ui/screens/MessageScreen.kt
@@ -3,12 +3,9 @@ package com.example.topics2.ui.screens
 import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -22,21 +19,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.navigation.NavController
 import com.example.topics.utilities.determineFileType
 import com.example.topics2.db.entities.FileInfoWithIcon
 import com.example.topics2.ui.components.CustomSearchBox
-import com.example.topics2.ui.components.global.CustomTextBox
 import com.example.topics2.ui.components.global.chooseColorBasedOnLuminance
 import com.example.topics2.ui.components.messageScreen.InputBarMessageScreen
 import com.example.topics2.ui.components.messageScreen.MessageBubble
@@ -70,7 +62,6 @@ fun MessageScreen(navController: NavController, viewModel: MessageViewModel, top
     val searchResults by viewModel.searchResults.observeAsState(emptyList())
     var searchResultCount: Int by remember { mutableStateOf(0 ) }
 
-    Log.d("QQWWEET", searchResultCount.toString())
     fun scrollMessage()
     {
         if ((searchResults.isEmpty()) || (searchResultCount >= searchResults.size) || (searchResultCount < 0)) {
@@ -242,7 +233,9 @@ fun MessageScreen(navController: NavController, viewModel: MessageViewModel, top
     }
     BackHandler {
         showSearchNav = false
-        viewModel.setSearchResultEmpty()
+        viewModel.setTopicId(-1)
+        viewModel.clearMessages()
+        viewModel.clearSearchResult()
         searchResultCount = 0
         inputText = ""
         navController.popBackStack()
