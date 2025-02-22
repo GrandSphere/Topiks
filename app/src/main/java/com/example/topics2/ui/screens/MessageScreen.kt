067c473d87e32b794a936b5a39b49f29f4ee0b8e
ichheise <ichheiseai@proton.me>
2025-02-22 12:03:15 +0200

    fixed deleting dialog

--- app/src/main/java/com/example/topics2/ui/screens/MessageScreen.kt
+++ app/src/main/java/com/example/topics2/ui/screens/MessageScreen.kt
@@ -1,12 +1,18 @@
 package com.example.topics2.ui.screens
 
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.minimumInteractiveComponentSize
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -18,7 +24,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
@@ -36,6 +44,7 @@ import com.example.topics2.ui.viewmodels.GlobalViewModelHolder
 import com.example.topics2.ui.viewmodels.MenuItem
 import com.example.topics2.ui.viewmodels.MessageViewModel
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.selects.select
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -50,6 +59,8 @@ fun MessageScreen(navController: NavController, viewModel: MessageViewModel, top
     val messages by viewModel.messages.collectAsState()
     var inputBarHeightPx by remember { mutableStateOf(0) }
 
+    var selectMultiple: Boolean by remember { mutableStateOf(false) }
+    val colors = MaterialTheme.colorScheme
     val coroutineScope = rememberCoroutineScope() // this should be passed from messagescreen
     val scrollState = rememberLazyListState()
     val topicFontColor = chooseColorBasedOnLuminance(topicColor)
@@ -73,10 +84,14 @@ fun MessageScreen(navController: NavController, viewModel: MessageViewModel, top
                     bSearch = !bSearch
 //                    coroutineScope.launch { ExportDatabaseWithPicker(context) }
                 },
+                MenuItem("Select Messages") {
+                    selectMultiple= !selectMultiple
+                },
                 MenuItem("Back") {
                     navController.popBackStack()
-                }
-            )
+                },
+
+                )
         )
 
     }
@@ -203,32 +218,55 @@ fun MessageScreen(navController: NavController, viewModel: MessageViewModel, top
                 ).format(message.createTime)
 
                 // Call MessageBubble
-                MessageBubble(
-                    navController = navController,
-                    topicColor = topicColor,
-                    topicFontColor = topicFontColor,
-                    messageContent = message.content,
-                    containsPictures = hasPictures,
-                    containsAttachments = hasAttachments,
-                    listOfPictures = pictureList,
-                    listOfAttachmentsP = attachmentList,
-                    timestamp = timestamp,
-                    onDeleteClick = {
-                        coroutineScope.launch {
-                            viewModel.deleteMessage(message.id)
-                        }
-                    },
-                    onViewMessage = {
-                        //TemporaryDataHolder.setMessage(message.content)
-
-                        viewModel.setTempMessageId(message.id)
-                        navController.navigate("navViewMessage")
-                    },
-                    onEditClick = {
-                        viewModel.setTempMessageId(message.id)
-                        viewModel.setEditMode(true)
+                var messageChecked: Boolean by remember { mutableStateOf(false) }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start ,
+                    modifier = Modifier
+                        .padding(0.dp)
+                ) {
+                    if (selectMultiple) {
+                        Checkbox(
+                            checked = messageChecked,
+                            onCheckedChange = { messageChecked = it },
+                            modifier = Modifier
+                                .scale(0.75f)
+                                .padding(start = 10.dp, end = 4.dp, bottom = 0.dp, top = 0.dp)
+                                .size(1.dp),
+                            colors = CheckboxDefaults.colors(
+                                checkedColor = topicColor,
+                                uncheckedColor = colors.onBackground,
+                                checkmarkColor = topicFontColor,
+                            )
+                        )
                     }
-                )
+                    MessageBubble(
+                        navController = navController,
+                        topicColor = topicColor,
+                        topicFontColor = topicFontColor,
+                        messageContent = message.content,
+                        containsPictures = hasPictures,
+                        containsAttachments = hasAttachments,
+                        listOfPictures = pictureList,
+                        listOfAttachmentsP = attachmentList,
+                        timestamp = timestamp,
+                        onDeleteClick = {
+                            coroutineScope.launch {
+                                viewModel.deleteMessage(message.id)
+                            }
+                        },
+                        onViewMessage = {
+                            //TemporaryDataHolder.setMessage(message.content)
+
+                            viewModel.setTempMessageId(message.id)
+                            navController.navigate("navViewMessage")
+                        },
+                        onEditClick = {
+                            viewModel.setTempMessageId(message.id)
+                            viewModel.setEditMode(true)
+                        }
+                    )
+                }
             }
         }
 
